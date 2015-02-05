20c0b897e475ff28b82ad9a316e1d1abef5d1d80
sim5 <sim5@gmu.edu>
2015-02-05 09:42:56 -0500

    first game
    prototype

--- js/main.js
+++ js/main.js
@@ -1,52 +1,266 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+EnemyTank = function (index, game, player) {
+
+    var x = game.world.randomX;
+    var y = game.world.randomY;
+
+    this.game = game;
+    this.health = 1;
+    this.player = player;
+  
+    this.fireRate = 1000;
+    this.nextFire = 0;
+    this.alive = true;
+
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
+    this.tank = game.add.sprite(x, y, 'enemy', 'tank1');
+   
+   
+
+ 
+    this.tank.anchor.set(0.5);
+
+	
+
+    this.tank.name = index.toString();
+    game.physics.enable(this.tank, Phaser.Physics.ARCADE);
+    this.tank.body.immovable = false;
+    this.tank.body.collideWorldBounds = true;
+    this.tank.body.bounce.setTo(1, 1);
+
+    this.tank.angle = game.rnd.angle();
+
+    game.physics.arcade.velocityFromRotation(this.tank.rotation, 1000, this.tank.body.velocity);
+
 };
+
+EnemyTank.prototype.damage = function() {
+
+    this.health -= 1;
+
+    if (this.health <= 0)
+    {
+        this.alive = false;
+
+      
+        this.tank.kill();
+      
+	  
+
+        return true;
+    }
+
+    return false;
+
+}
+
+EnemyTank.prototype.update = function() {
+
+    
+
+
+
+    if (this.game.physics.arcade.distanceBetween(this.tank, this.player) < 300)
+    {
+        if (this.game.time.now > this.nextFire )
+        {
+            this.nextFire = this.game.time.now + this.fireRate;
+
+          
+
+            
+
+            
+        }
+    }
+
+};
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload () {
+
+    game.load.image('tank', 'assets/owner.png');
+    game.load.image('enemy', 'assets/dogg.png');
+  
+
+    game.load.image('earth', 'assets/light_grass.png');
+    game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64, 23);
+	game.load.audio('kart', 'assets/kart.mp3');
+    
+}
+
+var land;
+
+
+var tank;
+
+
+var enemies;
+
+var enemiesTotal = 0;
+var enemiesAlive = 0;
+var explosions;
+
+
+
+var currentSpeed = 0;
+var cursors;
+
+
+var fireRate = 100;
+var nextFire = 0;
+
+function create () {
+
+    //  Resize our game world to be a 2000 x 2000 square
+    game.world.setBounds(-1000, -1000, 4000, 4000);
+	kart = game.add.audio('kart');
+        kart.loop = true;
+        kart.play();
+    //  Our tiled scrolling background
+    land = game.add.tileSprite(0, 0, 800, 600, 'earth');
+    land.fixedToCamera = true;
+
+    //  The base of our tank
+    tank = game.add.sprite(0, 0, 'tank', 'tank1');
+    tank.anchor.setTo(0.5, 0.5);
+    tank.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
+
+    //  This will force it to decelerate and limit its speed
+    game.physics.enable(tank, Phaser.Physics.ARCADE);
+    tank.body.drag.set(0.2);
+    tank.body.maxVelocity.setTo(1000, 1000);
+    tank.body.collideWorldBounds = true;
+
+   
+
+   
+
+    //  Create some baddies to waste :)
+    enemies = [];
+
+    enemiesTotal =10;
+    enemiesAlive = 10;
+
+    for (var i = 0; i < enemiesTotal; i++)
+    {
+        enemies.push(new EnemyTank(i, game, tank));
+    }
+
+   
+    
+
+    //  Explosion pool
+    explosions = game.add.group();
+
+    for (var i = 0; i < 10; i++)
+    {
+        var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
+        explosionAnimation.anchor.setTo(0.5, 0.5);
+        explosionAnimation.animations.add('kaboom');
+    }
+
+    tank.bringToTop();
+   
+
+ 
+
+
+
+    game.camera.follow(tank);
+    game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
+    game.camera.focusOnXY(0, 0);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+}
+
+
+
+function update () {
+
+    
+
+
+
+    for (var i = 0; i < enemies.length; i++)
+    {
+        if (enemies[i].alive)
+        {
+            
+           game.physics.arcade.overlap(tank, enemies[i].tank, playerHitDog, null, this);
+           
+            enemies[i].update();
+        }
+    }
+
+    if (cursors.left.isDown)
+    {
+        tank.angle -= 4;
+    }
+    else if (cursors.right.isDown)
+    {
+        tank.angle += 4;
+    }
+
+    if (cursors.up.isDown)
+    {
+        //  The speed we'll travel at
+        currentSpeed = 300;
+    }
+    else
+    {
+        if (currentSpeed > 0)
+        {
+            currentSpeed -= 4;
+        }
+    }
+
+    if (currentSpeed > 0)
+    {
+        game.physics.arcade.velocityFromRotation(tank.rotation, currentSpeed, tank.body.velocity);
+    }
+
+    land.tilePosition.x = -game.camera.x;
+    land.tilePosition.y = -game.camera.y;
+
+
+
+    
+
+    if (game.input.activePointer.isDown)
+    {
+        //  Boom!
+        fire();
+    }
+
+}
+
+
+function playerHitDog (tank, enemies) {
+
+enemiesAlive--;
+enemies.alive=false;
+enemies.kill();
+
+}
+
+
+
+function fire () {
+
+    if (game.time.now > nextFire)
+    {
+        nextFire = game.time.now + fireRate;
+
+        
+    }
+
+}
+
+function render () {
+
+    // game.debug.text('Active Bullets: ' + bullets.countLiving() + ' / ' + bullets.length, 32, 32);
+    game.debug.text('Dogs: ' + enemiesAlive + ' / ' + enemiesTotal, 300, 32);
+
+}
